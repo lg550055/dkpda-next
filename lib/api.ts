539ffbf9c85246efe5b3886d4bd605a58e6cff28
polo <lg550055@hotmail.com>
2025-11-03 11:46:35 -0800

    Sets proxy to 'false' to avoid calling deprecated url.parse() api

--- lib/api.ts
+++ lib/api.ts
@@ -4,6 +4,7 @@ import { getAuthToken } from "./auth";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
   withCredentials: true,
+  proxy: false,
 });
 
 api.interceptors.request.use((config) => {
