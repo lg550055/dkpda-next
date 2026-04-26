90bd6f39f6fb5095e5f5e61d97b11f36230a89f0
polo <lg550055@hotmail.com>
2026-04-25 19:43:39 -0700

    Adds 'force-dynamic'; avoid unavailable backedn API at build time

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,8 @@
 import ArticleForm from "@/components/ArticleForm";
 import api from "@/lib/api";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Admin() {
   const res = await api.get("/articles");
   const articles = res.data;
