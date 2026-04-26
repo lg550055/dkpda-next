a847f984f630ada6805d1d86d822a07766586736
polo <lg550055@hotmail.com>
2026-04-25 19:27:13 -0700

    Adds placeholder code to prevent deploymnet failure

---
+++ app/articles/page.tsx
@@ -0,0 +1,9 @@
+// Placeholder code; avoids deployment failure due to missing page component
+export default function ArticlesPage() {
+  return (
+    <div>
+      <h1>Articles</h1>
+      <p>This is the articles page.</p>
+    </div>
+  );
+}
